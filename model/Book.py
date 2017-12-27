8ea5f8951724d51e729160ed60b5584c2d2b7a42
marianka <mania600@gmail.com>
2017-12-28 00:19:13 +0100

    New packages. Book class in model package

---
+++ model/Book.py
@@ -0,0 +1,14 @@
+class Book(object):
+    def __init__(self):
+        self.__title = None
+        self.__author = None
+        self.__publishing_house = None
+        self.__publishing_date = None
+        self.__ISBN = None
+        self.__pages = None
+        self.__category = None
+        self.__tags = None
+        self.__borrow_info = None
+        self.__image = None
+        self.__place = None
+
